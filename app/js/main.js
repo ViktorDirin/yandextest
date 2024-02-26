8923fd7f2a58e52042dba83b40c11e6eddb5a504
Viktor Dirin <vdirin@gmail.com>
2024-02-26 16:08:24 +0400

    added adaptation to 375px, added dist folder

--- app/js/main.js
+++ app/js/main.js
@@ -1,33 +1,61 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 $(document).ready(function () {
     $(".users__slider").slick({
         // dots: true,
-
         arrows: true,
-        infinite: false,
+        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
+        // autoplay: true,
+        // autoplaySpeed: 4000,
         responsive: [
             {
                 breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                },
-            },
-            {
-                breakpoint: 768,
                 settings: {
                     slidesToShow: 2,
                 },
             },
             {
-                breakpoint: 576,
+                breakpoint: 768,
                 settings: {
                     slidesToShow: 1,
                 },
             },
         ],
     });
-    // $let('ttt') = $('.users__slider').slick('slickCurrentSlide');
-    // // $('.your-element').slick('setPosition');
-    // console.log($ttt);
+    $(".stages__steps").slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: "unslick"
+            }
+        ]
+
+    });
+
+    $('.btn-modal').on('click', function () {
+        console.log('click');
+        $('.modal').css("display", "block");
+    });
+    $('.closeModal, .modal').on('click', function () {
+        $('.modal').css("display", "none");
+    });
+
 });
+
